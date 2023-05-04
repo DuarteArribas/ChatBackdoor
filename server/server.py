2d17b56bd6a9f6805901b402be8d9ab46d5e46f6
DuarteArribas <duarte.a.arribas@gmail.com>
2023-05-04 10:23:18 +0100

    added server driver

--- server/server.py
+++ server/server.py
@@ -1,8 +1,11 @@
 from configparser import ConfigParser
+from server.src.chatServer import *
 
 def main():
   # read configuration from config file
-  cfg = ConfigParser("config/serverconf.cfg")
+  cfg    = ConfigParser("config/serverconf.cfg")
+  server = ChatServer(cfg.get("APP","IP"),cfg.get("APP","PORT"),cfg.get("APP","MAX_CLIENTS"))
+  server.runServer()
 
 if __name__ == '__main__':
   main()
