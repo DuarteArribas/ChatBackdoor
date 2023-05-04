2482882b3a7b2c5e8cdd12033e7ec4245dd70207
DuarteArribas <duarte.a.arribas@gmail.com>
2023-05-04 10:26:41 +0100

    added simple server/client communication

--- client/client.py
+++ client/client.py
@@ -1,8 +1,14 @@
 from configparser import ConfigParser
+from client.src.chatClient import *
 
 def main():
   # read configuration from config file
   cfg = ConfigParser("config/clientconf.cfg")
-
+  client = ChatClient(
+    cfg.get("APP","IP"),
+    cfg.get("APP","PORT")
+  )
+  client.runClient("a",["test.txt","test.txt"])
+  
 if __name__ == '__main__':
   main()
--- server/server.py
+++ server/server.py
@@ -4,7 +4,11 @@ from server.src.chatServer import *
 def main():
   # read configuration from config file
   cfg    = ConfigParser("config/serverconf.cfg")
-  server = ChatServer(cfg.get("APP","IP"),cfg.get("APP","PORT"),cfg.get("APP","MAX_CLIENTS"))
+  server = ChatServer(
+    cfg.get("APP","IP"),
+    cfg.get("APP","PORT"),
+    cfg.get("APP","MAX_CLIENTS")
+  )
   server.runServer()
 
 if __name__ == '__main__':
