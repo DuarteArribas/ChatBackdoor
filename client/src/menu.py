71daad2778868d8104816c0073d07b0284169f25
DuarteArribas <duarte.a.arribas@gmail.com>
2023-05-16 10:08:12 +0100

    added menus and tables

--- client/src/menu.py
+++ client/src/menu.py
@@ -1,5 +1,7 @@
 class Menu():
   INITIAL_MENU_OPTIONS = [0,1,2]
+  MAIN_MENU_OPTIONS    = [0,1,2,3]
+  FRIEND_MENU_OPTIONS = [0,1,2,3]
   
   @staticmethod
   def printInitialMenu():
@@ -24,4 +26,56 @@ class Menu():
         break
       print(f"Please enter a valid option ({Menu.INITIAL_MENU_OPTIONS[0]}-{Menu.INITIAL_MENU_OPTIONS[-1]})!")
       Menu.printInitialMenu()
+    return option
+  
+  @staticmethod
+  def printMainMenu():
+    print("\n=============================================")
+    print("Welcome to your main page! Select an option!")
+    print("1 - Friend list")
+    print("2 - Message a friend")
+    print("3 - Check messages")
+    print("0 - Exit")
+    print("=============================================")
+    
+  @staticmethod
+  def getMainMenuOption():
+    option = -1
+    while True:
+      try:
+        option = int(input("Option: "))
+      except ValueError:
+        print(f"Please enter a valid option ({Menu.MAIN_MENU_OPTIONS[0]}-{Menu.MAIN_MENU_OPTIONS[-1]})!")
+        Menu.printMainMenu()
+        continue
+      if option in Menu.MAIN_MENU_OPTIONS:
+        break
+      print(f"Please enter a valid option ({Menu.MAIN_MENU_OPTIONS[0]}-{Menu.MAIN_MENU_OPTIONS[-1]})!")
+      Menu.printMainMenu()
+    return option
+
+  @staticmethod
+  def printFriendMenu():
+    print("\n=============================================")
+    print("Welcome to your friends page! Which friends are you choosing today?")
+    print("1 - Add a friend")
+    print("2 - Friend request")
+    print("3 - Remove friend request")
+    print("0 - Exit")
+    print("=============================================")
+  
+  @staticmethod
+  def getFriendMenuOption():
+    option = -1
+    while True:
+      try:
+        option = int(input("Option: "))
+      except ValueError:
+        print(f"Please enter a valid option ({Menu.FRIEND_MENU_OPTIONS[0]}-{Menu.FRIEND_MENU_OPTIONS[-1]})!")
+        Menu.printFriendMenu()
+        continue
+      if option in Menu.FRIEND_MENU_OPTIONS:
+        break
+      print(f"Please enter a valid option ({Menu.FRIEND_MENU_OPTIONS[0]}-{Menu.FRIEND_MENU_OPTIONS[-1]})!")
+      Menu.printFriendMenu()
     return option
